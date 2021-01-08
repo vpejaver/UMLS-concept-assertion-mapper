b892527e9b120fcd01f850e963dcb2aa8534e2b9
vpejaver <vpejaver@uw.edu>
2021-01-07 23:43:21 -0800

    Added Java command to run pipeline to the README

--- PipelineScripts/src/main/java/edu/uw/bhi/bionlp/pipeline/Test.java
+++ PipelineScripts/src/main/java/edu/uw/bhi/bionlp/pipeline/Test.java
@@ -47,8 +47,6 @@ public class Test {
 
     public static void processNotes(long start, long end, IMetamapParser m, DataAccess da_MIMIC, DataAccess da_MIMIC_NLP, String dbTable, boolean verbose) throws Exception {
 	List<ClinicalNote> notes = da_MIMIC.getNoteData("select ROW_ID, NOTE_TEXT from " + dbTable  + " where ROW_ID between " + start + " and " + end + ";");
-        //List<ClinicalNote> notes = da_MIMIC.getNoteData("select ROW_ID, text from NOTEEVENTS where ROW_ID between " + start + " and " + end + ";");
-	//List<ClinicalNote> notes = da_MIMIC.getNoteData("select ROW_ID, NOTE_TEXT from CLINICAL_NOTE where ROW_ID between " + start + " and " + end + ";");
 	
         for (int nID = 0; nID < notes.size(); nID++) {
             String note = notes.get(nID).getText();
@@ -60,12 +58,9 @@ public class Test {
 		long cStart = System.currentTimeMillis(); // ADDED BY VIKAS
 		String gID = UUID.nameUUIDFromBytes(sentence.getBytes()).toString(); // ADDED BY VIKAS
 		long cEnd = System.currentTimeMillis(); // ADDED BY VIKAS
-                //da_MIMIC_NLP.insertSentence(noteID, sID, sentence);
 
 		long pStart = System.currentTimeMillis(); // ADDED BY VIKAS
-		//List<OutputConcept> processed = da_MIMIC_NLP.matchGUID(gID);
 		List<HashPointer> processed = da_MIMIC_NLP.matchGUIDFast(gID);
-		//List<HashPointer> processed = new ArrayList<HashPointer>();
 		
                 if (processed.isEmpty()) { // ADDED BY VIKAS
 		    List<UMLSConcept> concepts = m.parseSentenceWithMetamap(sentence); 
@@ -73,7 +68,6 @@ public class Test {
 			System.out.println("***************");
 		    }
 		    int k = 0;
-		    //System.out.println("@\t" + noteID + "\t" + concepts.size());		    
 		    for (UMLSConcept concept : concepts) {
 			String prediction = "-";
 			try {
@@ -137,77 +131,22 @@ public class Test {
             da_MIMIC_NLP.checkpointing(noteID, sentences.size(), startTime, endTime);
         }
     }
-
-    // multi processing
-    /*public static void processNotesMP(IMetamapParser m, DataAccess da_MIMIC, DataAccess da_MIMIC_NLP, String dbServer, String dbName, String dbTable,  boolean verbose, int nProc, String heapSize, String username, String password) throws Exception {
-        /*long total = da_MIMIC.getLongData("select count(*) from NOTEEVENTS;");
-        long minID = da_MIMIC.getLongData("select min(ROW_ID) from NOTEEVENTS;");
-        long maxID = da_MIMIC.getLongData("select max(ROW_ID) from NOTEEVENTS;");
-	long total = da_MIMIC.getLongData("select count(*) from CLINICAL_NOTE;");
-        long minID = da_MIMIC.getLongData("select min(ROW_ID) from CLINICAL_NOTE;");
-        long maxID = da_MIMIC.getLongData("select max(ROW_ID) from CLINICAL_NOTE;");
-	long total = da_MIMIC.getLongData("select count(*) from " + dbTable + ";");
-        long minID = da_MIMIC.getLongData("select min(ROW_ID) from " + dbTable + ";");
-        long maxID = da_MIMIC.getLongData("select max(ROW_ID) from " + dbTable + ";");
-	
-        int chunkSize = (int) Math.floorDiv(total, nProc-1); // CHANGED BY VIKAS TO COMMANDLINE PARAMETER
-        long numOfChunks = Math.floorDiv(total, chunkSize);
-        long remainder = Math.floorMod(total, chunkSize);
-        String classPath = System.getProperty("java.class.path");
-	List<Process> processes = new ArrayList<Process>(nProc);
-	
-        for (long cidx = minID; cidx < numOfChunks * chunkSize; cidx += chunkSize) {
-            final long startIdx = cidx;
-            final long endIdx = cidx + chunkSize - 1;
-	    List<String> command = Arrays.asList(new String[]{"java", "-Xmx" + heapSize, "-cp", classPath, "edu.uw.bhi.bionlp.pipeline.Test", dbServer, dbName, dbTable, "mml", "0", verbose ? "1" : "0", "1", heapSize, "" + startIdx, "" + endIdx, username, password});
-            System.out.println("processing notes from " + startIdx + " - " + endIdx + " ..." + command);
-            ProcessBuilder builder = new ProcessBuilder(command).inheritIO();
-            processes.add(builder.start());
-        }
-        if (remainder > 0) {
-            List<String> command = Arrays.asList(new String[]{"java", "-Xmx" + heapSize, "-cp", classPath, "edu.uw.bhi.bionlp.pipeline.Test", dbServer, dbName, dbTable, "mml", "0", verbose ? "1" : "0", "1", heapSize, "" + (numOfChunks * chunkSize), "" + maxID, username, password});
-            System.out.println("processing notes from " + numOfChunks * chunkSize + " - " + maxID + " ..." + command);
-            ProcessBuilder builder = new ProcessBuilder(command).inheritIO();
-	    Process process = builder.start();
-	    processes.add(builder.start());
-        }
-
-	// Wait for all processes to complete (ADDED BY VIKAS)
-	int children = nProc;
-	while (children != 0) {
-	    children = 0;
-	    for (Iterator<Process> iter = processes.iterator(); iter.hasNext();) {
-		children += iter.next().isAlive() ? 1 : 0;
-	    }
-	    //System.out.println(children);
-	}
-    }*/
-
   
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
         String dbServer = (args.length > 0) ? args[0] : "127.0.0.1";
         String nlpDB = (args.length > 1) ? args[1] : "mimic3_test"; //"UW_Note";
 	String dbTable = (args.length > 2) ? args[2] : "NOTEEVENTS"; //"CLINICAL_NOTE";
-        //boolean useMetaMap = (args.length > 3) && args[3].equalsIgnoreCase("mm");
-        //int notesCount = (args.length > 4) ? Integer.parseInt(args[4]) : 1;
         final boolean verbose = (args.length > 3) ? args[3].equalsIgnoreCase("1") : false;
-	//int nProc = (args.length > 6) ? Integer.parseInt(args[6]) : 10;
 	String heapSize = (args.length > 4) ? args[4] : "4g";
         int startNoteID = (args.length > 5) ? Integer.parseInt(args[5]) : -1;
         int endNoteID = (args.length > 6) ? Integer.parseInt(args[6]) : -1;
 	String username = (args.length > 7) ? args[7] : "";
 	String password = (args.length > 8) ? args[8] : "";
-        //boolean clearDB = (args.length > 9) && args[9].equalsIgnoreCase("clearDB");
-	//int TRIGFLAG = 3;
-	//int PROCFLAG = 4;
-	//int COMPFLAG = 1;
-	//int ERRFLAG = 0;
 	final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	final Date date = new Date();		
 	final DataAccess da_MIMIC = new DataAccess();
 	final DataAccess da_MIMIC_NLP = new DataAccess();
-	//int flag = 0;
 	
         try {
 	    if (password.equals("")) {
@@ -217,44 +156,11 @@ public class Test {
 		da_MIMIC.SetupConnection(dbServer, nlpDB, username, password); // Corrected by Vikas    
 		da_MIMIC_NLP.SetupConnection(dbServer, nlpDB, username, password);
 	    }
-            /*if (clearDB) {
-                da_MIMIC_NLP.clearDB();
-                return;
-		}*/
-	    
-            //IMetamapParser m = useMetaMap ? new MetamapParser_ITHS() : new MetamapLiteParser();
+
 	    IMetamapParser m = new MetamapLiteParser();
 	    
-            /*if (notesCount > 0) {
-                processNotes(0, 1, m, da_MIMIC, da_MIMIC_NLP, dbTable, verbose);
-		} else if (startNoteID > 0) {*/
-                processNotes(startNoteID, endNoteID, m, da_MIMIC, da_MIMIC_NLP, dbTable, verbose);
-		/*} else {
-		// DO OPERATIONS CHECKS/UPDATES HERE BECAUSE WE CARE MOSTLY ABOUT MULTI-PROCESS
-
-		// Process if status = 3 (NOTE_TRANSFER_COMPLETE)
-		flag = da_MIMIC_NLP.getDBStatus();
-		System.out.println("#### STATUS ####\t" + flag + "\t" + dateFormat.format(date));
-		if (flag != TRIGFLAG) {
-		    return;
-		}
-
-		// Update status to 4 (PROCESSING_NOTES)
-		da_MIMIC_NLP.setDBStatus(PROCFLAG, "vpejaver");
-		flag = da_MIMIC_NLP.getDBStatus();
-		System.out.println("#### STATUS ####\t" + flag + "\t" + dateFormat.format(date));
-
-                processNotesMP(m, da_MIMIC, da_MIMIC_NLP, dbServer, nlpDB, dbTable, verbose, nProc, heapSize, username, password);
-
-		// Update status to 1 (WAITING_FOR_NEW_NOTES)
-		da_MIMIC_NLP.setDBStatus(COMPFLAG, "vpejaver");
-		flag = da_MIMIC_NLP.getDBStatus();
-		System.out.println("#### STATUS ####\t" + flag + "\t" + dateFormat.format(date));
-		}*/
+	    processNotes(startNoteID, endNoteID, m, da_MIMIC, da_MIMIC_NLP, dbTable, verbose);
         } catch (Exception ex) {
-	    //da_MIMIC_NLP.setDBStatus(ERRFLAG, "vpejaver");
-	    //flag = da_MIMIC_NLP.getDBStatus();
-	    //System.out.println("#### STATUS ####\t" + flag + "\t" + dateFormat.format(date));
             Logger.getLogger("PipelineScripts").log(Level.SEVERE, null, ex);
         }
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # UMLS-concept-assertion-mapper
+## Setting up the input as a MySQL database
+
+## Running the pipeline
+To run the main file (Test.java in src/main/java/edu/uw/bhi/bionlp/pipeline), change directory to *PipelineScripts* and then type:
+```java -Xmx4g -Dlog4j.configurationFile=file:////home/vpejaver/repos/UMLS-concept-assertion-mapper/PipelineScripts/src/main/resources/log4j2.xml -cp "src/main/java:src/main/resources:lib/*" edu.uw.bhi.bionlp.pipeline.Test <DB server> <DB name> <Table name> <Verbose> <Max heap size> <Start note ID> <End note ID> <Username> <Password>
+```
+where,
+* DB server can be localhost or an IP address of the server hosting the input MySQL table
+* DB name is the name of the MySQL database
+* Table name is the name of the table containing input data
+* Verbose is a Boolean (0 or 1) indicating the need for verbose outputs
+* Max heap size is the maximum heap size for Java (determines memory usage), typically an integer followed by g, m or k
+* Start note ID is the ID number of the first report to be processed
+* End note ID is the ID number of the last report to be processed
+* Username is the username for the MySQL database
+* Password is the password for access to this database
+
+Here is an example:
+```java -Xmx4g -Dlog4j.configurationFile=file:////home/vpejaver/repos/UMLS-concept-assertion-mapper/PipelineScripts/src/main/resources/log4j2.xml -cp "src/main/java:src/main/resources:lib/*" edu.uw.bhi.bionlp.pipeline.Test localhost CLEAR TAN_DATASET_SEG_OP 0 4g 1 871 vpejaver password
+```
